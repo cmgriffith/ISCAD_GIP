248197a85e93b22b1ee1ce2ad59d46cc50ff9799
cmgriffith <86885235+cmgriffith@users.noreply.github.com>
2026-01-25 23:17:23 +0000

    Added via github.com upload
    test before main push

--- ISCAD_Analytical.py
+++ ISCAD_Analytical.py
@@ -1,95 +1,44 @@
 import numpy as np
 from scipy import constants as cons
-import json
-import sys
+from functions import *
 # import matplotlib.pyplot as plt
-
 import warnings
 warnings.filterwarnings('ignore')
 
-# Load parameters from JSON or Python file
-use_json = '--json' in sys.argv or len(sys.argv) > 1 and sys.argv[1].endswith('.json')
+# load parameters from .json
+file = 'ISCAD_parameters.json'
+params = loadjson(file)
 
-if use_json:
-    # Load from JSON file
-    json_file = 'ISCAD_parameters.json'
-    if len(sys.argv) > 1 and sys.argv[1].endswith('.json'):
-        json_file = sys.argv[1]
-    
-    with open(json_file, 'r') as f:
-        params = json.load(f)
-    
-    # Extract parameters
-    Qs = params['machine_config']['Qs']
-    Qs_active = params['machine_config']['Qs_active']
-    p = params['machine_config']['p']
-    Qr = params['machine_config']['Qr']
-    
-    ag = params['machine_dimensions']['ag']
-    l_stack = params['machine_dimensions']['l_stack']
-    r_o = params['machine_dimensions']['r_o']
-    r_so = params['machine_dimensions']['r_so']
-    r = params['machine_dimensions']['r']
-    r_yoke = params['machine_dimensions']['r_yoke']
-    w_slot = np.array(params['machine_dimensions']['w_slot'])
-    w_so = params['machine_dimensions']['w_so']
-    h_slot = params['machine_dimensions']['h_slot']
-    h_so = params['machine_dimensions']['h_so']
-    w_bar = np.array(params['machine_dimensions']['w_bar'])
-    w_bo = params['machine_dimensions']['w_bo']
-    h_bar = params['machine_dimensions']['h_bar']
-    
-    rho_s = params['material_parameters']['rho_s']
-    
-    Apk = params['electrical_parameters']['Apk']
-    f = params['electrical_parameters']['f']
-    
-    k = params['analysis_parameters']['k']
-    
-    # Power electronics parameters (if available)
-    if 'power_electronics' in params:
-        V_DC = params['power_electronics']['V_DC']
-        f_SW = params['power_electronics']['f_SW']
-        V_DC_rip_max = params['power_electronics']['V_DC_rip_max']
-        Apk_PE = params['power_electronics']['Apk']
-    
-    print("Parameters loaded from JSON file")
-else:
-    # Load from Python file
-    from ISCAD_parameters import *
-    print("Parameters loaded from Python file")
 
 ### STATOR RESISTANCE
-Rs_DC = rho_s * l_stack / (h_slot * w_slot[0])
-zeta = ( (np.pi * cons.mu_0 * f) / rho_s )**0.5 * h_slot
-print("stator DC resistance = ", ("{:.2e}".format(Rs_DC)), "Ohm")
+Rs_DC = params.rho_s * params.l_stack / (params.h_slot * params.w_slot[0])
+zeta = ( (np.pi * cons.mu_0 * params.f) / params.rho_s )**0.5 * params.h_slot
+print("stator DC resistance = ", ("{:.3f}".format(Rs_DC*1e3)), "mOhm")
 K_R = zeta * (np.sinh(2*zeta) + np.sin(2*zeta)) / (np.cosh(2*zeta) - np.cos(2*zeta)) 
 Rs_AC = Rs_DC * K_R
-print("stator AC resistance = ", round(Rs_AC,1), "Ohm")
+print("stator AC resistance = ", ("{:.3f}".format(Rs_AC*1e3)), "mOhm")
 
 ### SELF INDUCTANCE
-
-Ls_s = np.pi/2 * cons.mu_0 * (r * l_stack / ag)
-print("self inductance = " , round(Ls_s,3), "H")
+Ls_s = np.pi/2 * cons.mu_0 * (params.r * params.l_stack / params.ag)
+print("self inductance = " , ("{:.3f}".format(Ls_s*1e3)), "mH")
 
 ### LEAKAGE INDUCTANCE
 # for DC current in rectangular slots
-lambda_slot = (h_slot / (3*w_slot[0])) + (h_so / w_so)
-Ls_sigma_DC = cons.mu_0 * l_stack * lambda_slot
-
-K_L = (3 / (2*zeta)) * (np.sinh(2*zeta) - np.sin(2*zeta)) / (np.cosh(2*zeta) - np.cos(2*zeta)) 
-Ls_sigma_AC = cons.mu_0 * l_stack * lambda_slot * K_L
-
-print("leakage inductance DC = " , round(Ls_sigma_DC,3), "H")
+lambda_slot = (params.h_slot / 3*params.w_slot[0]) + (params.h_so / params.w_so)
+Ls_sigma_DC = cons.mu_0 * params.l_stack * lambda_slot
+print("DC leakage inductance = " ,  ("{:.3f}".format(Ls_sigma_DC*1e6)), "uH")
+# AC corrected to fundamental f
+K_L = 3 / (2*zeta) * (np.sinh(2*zeta) - np.sin(2*zeta)) / (np.cosh(2*zeta) - np.cos(2*zeta)) 
+Ls_sigma_AC = cons.mu_0 * params.l_stack * lambda_slot * K_L
+print("AC leakage inductance = " ,  ("{:.3f}".format(Ls_sigma_AC*1e6)), "uH")
 
 
 ### MAIN INDUCTANCE
 sum = 0
-for k in range(1,round(Qs/2/p + 1)):
-    sum = sum + np.cos( p * (k-1) * 2*np.pi / Qs)**2
+for k in range(1,round(params.Qs/2/params.p + 1)):
+    sum = sum + np.cos( params.p * (k-1) * 2*np.pi / params.Qs)**2
 Ls_m = Ls_s + sum
-
-print("main inductance = " , round(Ls_m,3), "H")
+print("main inductance = " ,  ("{:.3f}".format(Ls_m*1e3)), "mH")
 
 
 '''
@@ -126,13 +75,10 @@ plt.show()
 
 # parameters [SI]
 # ----------------------USER INPUT-----------------------
-if not use_json or 'power_electronics' not in params:
-    V_DC = 48
-    f_SW = 25000
-    V_DC_rip_max = 2.6
-    Apk_PE = 140 # [30s,330A] [5s,500A]
-else:
-    Apk_PE = params['power_electronics']['Apk']
+V_DC = 48
+f_SW = 25000
+V_DC_rip_max = 2.6
+Apk = 140 # [30s,330A] [5s,500A] 
 # -------------------------------------------------------
 
-C_DC_min = Apk_PE / ( V_DC_rip_max * 2 * np.pi * f_SW )
+C_DC_min = Apk / ( params.V_DC_rip_max * 2 * np.pi * params.f_SW )
